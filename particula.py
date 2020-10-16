6759c9d73f4e083a7e091f589d53465d5f014a8a
Ramón García <ramon.garcia5800.alumnos.udg.mx>
2020-10-16 08:40:31 -0500

    Prueba01

---
+++ particula.py
@@ -0,0 +1,32 @@
+from algoritmos import distancia_euclidiana
+
+class Particula:
+	def __init__(self, 
+                id = 0, origen_x = 0, origen_y = 0,
+                destino_x = 0, destino_y = 0 , velocidad = 0,
+                red = 0 , green = 0 , blue = 0 , distancia = 0.0  ):
+            self.__id = id
+            self.__origen_x = origen_x 
+            self.__origen_y = origen_y
+            self.__destino_x = destino_x
+            self.__destino_y = destino_y
+            self.__red = red
+            self.__green = green
+            self.__blue = blue
+			#self.distancia = distancia_euclidiana(origen_x, destino_x, origen_y, destino_y)
+    
+    def __str__(self):
+        return (
+            'Id: ' + str(self.__id) + '\n' +
+            'Origen_x: ' + str(self.__origen_x) + '\n' +
+            'Origen_y: ' + str(self.__origen_y) + '\n' +
+            'Destino_x: ' + str(self.__destino_x) + '\n' +
+            'Destino_y: ' + str(self.__destino_y) + '\n' +
+            'Red: ' + str(self.__red) + '\n' +
+            'Green: ' + str(self.__green) + '\n' +
+            'Blue: ' + str(self.__blue) + '\n' +
+            #'Distancia: ' + self.distancia + '\n' 
+        )
+
+p01 = Particula("2", "0", "8", "16", "16", "1", "3", "5")
+print(p01)
